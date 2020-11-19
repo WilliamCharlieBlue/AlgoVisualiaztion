488bda75fbfa427b2f149b58e84b81f88c0ef730
tom_glb <jerry_glb@abc.com>
2020-11-20 00:30:19 +0800

    First Depth Search with stack using while-loop ,after getting solution,find the whole path using pre-position marked when pushing new-position in stack.

---
+++ MazeSolver-FDS-stack_loop/src/AlgoFrame.java
@@ -0,0 +1,86 @@
+import javafx.beans.binding.ObjectExpression;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AlgoFrame extends JFrame{
+    private int canvasWidth;
+    private int canvasHeight;
+
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight) {
+        super(title);
+
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        AlgoCanvas canvas = new AlgoCanvas();
+        setContentPane(canvas);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+    public AlgoFrame(String title){
+        this(title, 1024, 768);
+    }
+
+    public int getCanvasWidth(){return canvasWidth;}
+    public int getCanvasHeight(){return canvasHeight;}
+
+    // TODO: 设置自己的数据
+    private MazeData data;
+    public void render(MazeData data){
+        this.data = data;
+        repaint();
+    }
+
+    private class AlgoCanvas extends JPanel{
+        public AlgoCanvas(){
+            // 双缓存
+            super(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D)g;
+            //抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_DEFAULT);
+            g2d.addRenderingHints(hints);
+
+            // 具体绘制
+            // TODO： 绘制自己的数据data
+            int w = canvasWidth/data.M();
+            int h = canvasHeight/data.N();
+            for(int i = 0; i < data.N(); i++){
+                for(int j = 0; j < data.M(); j++){
+//                    if(data.getMaze(i,j) == '#')
+                    if(data.getMaze(i,j) == MazeData.WALL)
+                        AlgoVisHelper.setColor(g2d, AlgoVisHelper.LightBlue);
+                    else
+                        AlgoVisHelper.setColor(g2d, AlgoVisHelper.White);
+
+                    if(data.path[i][j] == true)
+                        AlgoVisHelper.setColor(g2d, AlgoVisHelper.Yellow);
+                    // 标记运行获得的解的路径
+                    if(data.result[i][j] == true)
+                        AlgoVisHelper.setColor(g2d, AlgoVisHelper.Red);
+                    // 横坐标是列数*列宽；纵坐标是行数*行高
+                    AlgoVisHelper.fillRectangle(g2d, j*w, i*h, w, h);
+                }
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize(){
+            return new Dimension(canvasWidth,canvasHeight);
+        }
+    }
+}
